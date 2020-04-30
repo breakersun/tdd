dd130cae02b45c01980c46f30e1963025b055d44
leo <leo@localhost.localdomain>
2020-04-30 08:12:22 +0800

    add random checking; use more test rounds to test randomized distritution.

---
+++ include/HomeAutomation/RandomMinute.h
@@ -0,0 +1,15 @@
+
+
+#ifndef D_RandomMinute_H
+#define D_RandomMinute_H
+
+
+void RandomMinute_Create(int bound);
+extern int (*RandomMinute_Get)(void);
+
+#if 0 
+void RandomMinute_Create(int bound);
+int RandomMinute_Get(void);
+#endif 
+
+#endif  /* D_RandomMinute_H */
