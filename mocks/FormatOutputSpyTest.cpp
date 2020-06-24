c15878512cd6b07fbfaaffe79e0e43c96906929e
leo <leo@localhost.localdomain>
2020-06-24 15:01:46 +0800

    finish simple test for FormatOutputSpy

--- mocks/FormatOutputSpyTest.cpp
+++ mocks/FormatOutputSpyTest.cpp
@@ -27,3 +27,25 @@ TEST(FormatOutputSpy, HelloWorld)
     FormatOutput("Hello, World\n");
     STRCMP_EQUAL("Hello, World\n", FormatOutputSpy_GetOutput());
 }
+
+TEST(FormatOutputSpy, ExceedTheOutputBufferSize)
+{
+    FormatOutputSpy_Create(4);
+    FormatOutput("Hello, World\n");
+    STRCMP_EQUAL("Hell", FormatOutputSpy_GetOutput());
+}
+TEST(FormatOutputSpy, PrintMultipleTimes)
+{
+    FormatOutputSpy_Create(20);
+    FormatOutput("Hello,");
+    
+    FormatOutput(" World\n");
+    STRCMP_EQUAL("Hello, World\n", FormatOutputSpy_GetOutput());
+}
+TEST(FormatOutputSpy, PrintMultipleTimesExceedBufferSize)
+{
+    FormatOutputSpy_Create(12);
+    FormatOutput("12345"); FormatOutput("67890");
+    FormatOutput("ABCD");
+    STRCMP_EQUAL("1234567890AB", FormatOutputSpy_GetOutput());
+}
