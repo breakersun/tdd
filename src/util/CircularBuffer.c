c5b986414741a844292144e21f3b82a20615795e
leo <leo@localhost.localdomain>
2020-06-24 17:22:14 +0800

    circularbuffer working on

--- src/util/CircularBuffer.c
+++ src/util/CircularBuffer.c
@@ -13,7 +13,7 @@ typedef struct CircularBufferStruct
     int * values;
 }CircularBufferSturct;
 
-enum {BUFFER_GUARD = -999};
+enum {BUFFER_GUARD = 0xDEADBEAF};
 
 CircularBuffer CircularBuffer_Create(int capacity)
 {
@@ -29,4 +29,38 @@ void CircularBuffer_Destroy(CircularBuffer self)
 {
     free(self->values);
     free(self);
+}
+
+int CircularBuffer_VerifyIntegrity(CircularBuffer self)
+{
+    return (self->values[self->capacity] == BUFFER_GUARD);
+}
+
+int CircularBuffer_IsEmpty(CircularBuffer self)
+{
+    return (self->count == 0);
+}
+
+int CircularBuffer_IsFull(CircularBuffer self)
+{
+    return (self->count == self->capacity);
+}
+
+int CircularBuffer_Put(CircularBuffer self, int value)
+{
+    self->count++;
+    self->values[self->index++] = value;
+
+    return 1;
+}
+
+int CircularBuffer_Get(CircularBuffer self)
+{
+    self->count--;
+    return (self->values[self->outdex++]);
+}
+
+int CircularBuffer_Capacity(CircularBuffer self)
+{
+    return self->capacity;
 }
--- tests/util/CircularBufferTest.cpp
+++ tests/util/CircularBufferTest.cpp
@@ -24,4 +24,109 @@ TEST_GROUP(CircularBuffer)
 
 TEST(CircularBuffer, Create)
 {
+    CHECK_TRUE(CircularBuffer_VerifyIntegrity(buffer));
+}
+
+TEST(CircularBuffer, EmptyAfterCreatiion)
+{
+    CHECK_TRUE(CircularBuffer_IsEmpty(buffer));
+}
+
+TEST(CircularBuffer, NotFullAfterCreation)
+{
+    CHECK_FALSE(CircularBuffer_IsFull(buffer));
+}
+
+TEST(CircularBuffer, NotEmptyAfterInput)
+{
+    CircularBuffer_Put(buffer, 10046);
+    CHECK_FALSE(CircularBuffer_IsEmpty(buffer));
+}
+
+TEST(CircularBuffer, NotEmptyThenEmpty)
+{
+    CircularBuffer_Put(buffer, 10046);
+    CHECK_FALSE(CircularBuffer_IsEmpty(buffer));
+    CircularBuffer_Get(buffer);
+    CHECK_TRUE(CircularBuffer_IsEmpty(buffer));
+}
+
+TEST(CircularBuffer, PutGetOneValue)
+{
+    CircularBuffer_Put(buffer, 10046);
+    LONGS_EQUAL(10046, CircularBuffer_Get(buffer));
+}
+
+TEST(CircularBuffer, PutGetAFew)
+{
+    CircularBuffer_Put(buffer, 10046);
+    CircularBuffer_Put(buffer, 10047);
+    CircularBuffer_Put(buffer, 10048);
+    LONGS_EQUAL(10046, CircularBuffer_Get(buffer));
+    LONGS_EQUAL(10047, CircularBuffer_Get(buffer));
+    LONGS_EQUAL(10048, CircularBuffer_Get(buffer));
+}
+
+TEST(CircularBuffer, Capacity)
+{
+    CircularBuffer b = CircularBuffer_Create(2);
+    LONGS_EQUAL(2, CircularBuffer_Capacity(b));
+    CircularBuffer_Destroy(b);
+}
+
+TEST(CircularBuffer, IsFull)
+{
+    for (int i = 0; i < CircularBuffer_Capacity(buffer); i++)
+        CircularBuffer_Put(buffer, i + 100);
+
+    CHECK_TRUE(CircularBuffer_IsFull(buffer));
+}
+
+TEST(CircularBuffer, EmptyToFullToEmpty)
+{
+    for (int i = 0; i < CircularBuffer_Capacity(buffer); i++)
+        CircularBuffer_Put(buffer, i + 100);
+
+    CHECK_TRUE(CircularBuffer_IsFull(buffer));
+
+    for (int j = 0; j < CircularBuffer_Capacity(buffer); j++)
+        LONGS_EQUAL(j + 100, CircularBuffer_Get(buffer));
+
+    CHECK_TRUE(CircularBuffer_IsEmpty(buffer));
+}
+
+TEST(CircularBuffer, WrapAround)
+{
+    int capacity = CircularBuffer_Capacity(buffer);
+
+    for (int i = 0; i < capacity; i++)
+        CircularBuffer_Put(buffer, i + 100);
+    
+    CHECK_TRUE(CircularBuffer_IsFull(buffer));
+    LONGS_EQUAL(100, CircularBuffer_Get(buffer));
+    CHECK_FALSE(CircularBuffer_IsFull(buffer));
+    CircularBuffer_Put(buffer, 1000);
+    CHECK_TRUE(CircularBuffer_IsFull(buffer));
+
+    for (int j = 1; j < capacity; j++)
+        LONGS_EQUAL(j + 100, CircularBuffer_Get(buffer));
+
+    LONGS_EQUAL(1000, CircularBuffer_Get(buffer));
+    CHECK_TRUE(CircularBuffer_IsEmpty(buffer));
+    CHECK_TRUE(CircularBuffer_VerifyIntegrity(buffer));
+}
+
+TEST(CircularBuffer, PutToFullThrows)
+{
+
+}
+
+TEST(CircularBuffer, PutToFullNotDamageContents)
+{
+
+}
+
+TEST(CircularBuffer, GetFromEmptyReturns0)
+{
+
 }
