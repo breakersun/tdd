81beb2bcba95e9998c9249b94f5d439c18731196
leo <leo@localhost.localdomain>
2020-06-24 14:35:41 +0800

    add a test for FormatOutputSpy

---
+++ mocks/FormatOutputSpyTest.cpp
@@ -0,0 +1,27 @@
+
+
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+// #include ""
+}
+
+TEST_GROUP(FormatOutputSpy)
+{
+    void setup()
+    {
+
+    }
+
+    void teardown()
+    {
+
+    }
+};
+
+
+TEST(FormatOutputSpy, HelloWorld)
+{
+
+}
