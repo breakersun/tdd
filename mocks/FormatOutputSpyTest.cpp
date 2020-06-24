2c15499baa18e5b7107c0789a821b968eabb4630
leo <leo@localhost.localdomain>
2020-06-24 14:55:01 +0800

    test FormatOutputSpy with hello world works fine

--- mocks/FormatOutputSpyTest.cpp
+++ mocks/FormatOutputSpyTest.cpp
@@ -4,24 +4,26 @@
 
 extern "C"
 {
-// #include ""
+#include "FormatOutputSpy.h"
 }
 
 TEST_GROUP(FormatOutputSpy)
 {
     void setup()
     {
-
+        UT_PTR_SET(FormatOutput, FormatOutputSpy);
     }
 
     void teardown()
     {
-
+        FormatOutputSpy_Destroy();
     }
 };
 
 
 TEST(FormatOutputSpy, HelloWorld)
 {
-
+    FormatOutputSpy_Create(20);
+    FormatOutput("Hello, World\n");
+    STRCMP_EQUAL("Hello, World\n", FormatOutputSpy_GetOutput());
 }
